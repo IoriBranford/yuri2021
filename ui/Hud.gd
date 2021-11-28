553b75edff8faa6c02f5b7f929988f6127ccc7a3
IoriBranford <ioribranford@gmail.com>
2021-11-28 01:09:40 -0800

    Lerp transformation meter color when increasing, and flash warning when nearly expired.

--- ui/Hud.gd
+++ ui/Hud.gd
@@ -2,7 +2,11 @@ extends Control
 
 export var girlfriend_time = 90
 export var transformation_time = 2
-export var transformation_revert_time = 10
+export var revert_time = 10
+export var revert_warning_timeleft = 2.5
+export var transformation_start_color = Color.aqua
+export var transformation_finish_color = Color.bisque
+export var revert_warning_color = Color.red
 
 signal transformation_finished
 signal transformation_canceled
@@ -10,7 +14,7 @@ signal transformation_timeout
 signal girlfriend_timeout
 
 func _ready():
-	$MyTransformationClock.max_value = transformation_revert_time
+	$MyTransformationClock.max_value = revert_time
 	$MyTransformationClock.progress = 0
 	$GirlfriendClock.max_value = girlfriend_time
 	$GirlfriendClock.progress = girlfriend_time
@@ -25,16 +29,20 @@ func _physics_process(delta):
 			mtc_progress = move_toward(mtc_progress, mtc_max, mtc_delta)
 			if mtc_progress >= mtc_max:
 				emit_signal("transformation_finished")
-				$MyTransformationTimer.start(transformation_revert_time)
+				$MyTransformationTimer.start(revert_time)
 		else:
 			mtc_progress = move_toward(mtc_progress, 0, mtc_delta)
 		$MyTransformationClock.progress = mtc_progress
+		$MyTransformationClock.bar_color = lerp(transformation_start_color, transformation_finish_color, mtc_progress/mtc_max)
 	else:
 		if Input.is_action_just_pressed("player_transform"):
 			emit_signal("transformation_canceled")
 			_on_cancel_transformation()
 		else:
-			$MyTransformationClock.progress = $MyTransformationTimer.time_left
+			var mtt_timeleft = $MyTransformationTimer.time_left
+			$MyTransformationClock.progress = mtt_timeleft
+			if mtt_timeleft <= revert_warning_timeleft:
+				$MyTransformationClock.bar_color = lerp(transformation_start_color, revert_warning_color, (1 + sin(6*PI*mtt_timeleft)) / 2)
 
 	$GirlfriendClock.progress = $GirlfriendTimer.time_left
 
