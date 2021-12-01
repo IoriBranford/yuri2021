8ad2abd8073987c905d70b684daacf2a4467d233
IoriBranford <ioribranford@gmail.com>
2021-12-01 06:18:03 -0800

    Start player enter/exit shop animation.

--- scripts/TestPlayer.gd
+++ scripts/TestPlayer.gd
@@ -4,22 +4,76 @@ export var speed = 2.0
 
 signal got_item(item)
 
+var current_physics_process = funcref(self, "_physics_process_input")
+var co_state = null
+var nearby_shop:Area = null
+var inv_items = {
+	stylist = false,
+	music = false,
+	gift = false
+}
+
+func connect_to_shop(nodepath:NodePath):
+	var shop:Area = get_node(nodepath) as Area
+	if shop:
+		shop.connect("body_entered", self, "_on_shoparea_body_entered", [shop], 0)
+		shop.connect("body_exited", self, "_on_shoparea_body_exited", [shop], 0)
+
 func _ready():
-	pass
+	connect_to_shop("../SalonArea")
+	connect_to_shop("../GiftShopArea")
+	connect_to_shop("../MusicArea")
 
-func _process(delta):
-	pass
-
-func _physics_process(delta):
+func _physics_process_input(delta):
 	var movement_x = Input.get_axis("player_left", "player_right")
 	move_and_slide(speed * Vector3(movement_x, 0, 0), Vector3.UP)
+	var scaley
 	if Input.is_action_pressed("player_hide"):
-		var scaley = move_toward(scale.y, .5, delta*4)
-		scale.y = scaley
+		scaley = move_toward(scale.y, .5, delta*4)
 	else:
-		var scaley = move_toward(scale.y, 1, delta*4)
-		scale.y = scaley
+		scaley = move_toward(scale.y, 1, delta*4)
+	scale.y = scaley
+	if Input.is_action_just_pressed("player_enter"):
+		visit_shop()
 
-func _on_SalonArea_body_entered(body):
+func co_visit_shop():
+	var delta
+	var x = translation.x
+	var shopx = nearby_shop.translation.x
+	while x != shopx:
+		delta = yield()
+		x = move_toward(x, shopx, delta*speed)
+		translation.x = x
+		
+	var mask = collision_mask
+	collision_mask = 0
+	$AnimationPlayer.play("enter_shop")
+	yield(get_tree(), "idle_frame")
+	yield($AnimationPlayer, "animation_finished")
+	$AnimationPlayer.play("exit_shop")
+	yield(get_tree(), "idle_frame")
+	yield($AnimationPlayer, "animation_finished")
+	current_physics_process = funcref(self, "_physics_process_input")
+	collision_mask = mask
+	
+func _physics_process_coroutine(delta):
+	if co_state is GDScriptFunctionState && co_state.is_valid():
+		co_state = co_state.resume(delta)
+	else:
+		co_state = null
+
+func visit_shop():
+	if nearby_shop:
+		co_state = co_visit_shop()
+		current_physics_process = funcref(self, "_physics_process_coroutine")
+
+func _physics_process(delta):
+	current_physics_process.call_func(delta)
+
+func _on_shoparea_body_entered(body, shop):
 	if self == body:
-		emit_signal("got_item", "stylist")
+		nearby_shop = shop
+
+func _on_shoparea_body_exited(body, shop):
+	if self == body and shop == nearby_shop:
+		nearby_shop = null
