3bd253ccd351cbcb84b23214971419530af877a5
IoriBranford <ioribranford@gmail.com>
2021-12-04 17:04:53 -0800

    Press attack to enter a shop.

--- scenes/PlayerObj.gd
+++ scenes/PlayerObj.gd
@@ -14,25 +14,20 @@ var move_dir = Vector3.ZERO
 var in_knockback = false
 var is_transformed = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	pass
-
 func move_player():
 	if in_knockback:
 		move_dir = Vector3.BACK
 	else:
-		move_dir = Vector3.ZERO
+		move_dir = Vector3(
+			Input.get_axis("ui_up", "ui_down"),
+			0,
+			Input.get_axis("ui_right", "ui_left"))
 		if Input.is_action_just_pressed("player_attack"):
-			melee_attack()
-		if Input.is_action_pressed("ui_left"):
-			move_dir += Vector3.BACK
-		if Input.is_action_pressed("ui_right"):
-			move_dir += Vector3.FORWARD
-		if Input.is_action_pressed("ui_up"):
-			move_dir += Vector3.LEFT
-		if Input.is_action_pressed("ui_down"):
-			move_dir += Vector3.RIGHT
+			if nearby_shop:
+				visit_shop(nearby_shop)
+				move_dir = Vector3.ZERO
+			else:
+				melee_attack()
 	if move_dir != Vector3.ZERO:
 		look_at(global_transform.origin + move_dir.normalized(), Vector3.UP)
 		move_dir = move_dir.normalized() * MOVE_SPEED
@@ -69,6 +64,8 @@ signal shop_entered(shop)
 signal shop_exited(shop)
 signal got_item(item)
 
+var co_state = null
+var nearby_shop:Area = null
 var visited_shops = {
 	Salon = false,
 	GiftShop = false,
@@ -89,6 +86,7 @@ func connect_to_shop(nodepath:NodePath):
 	var shop:Area = get_node(nodepath) as Area
 	if shop:
 		shop.connect("body_entered", self, "_on_shoparea_body_entered", [shop], 0)
+		shop.connect("body_exited", self, "_on_shoparea_body_exited", [shop], 0)
 
 func _ready():
 	connect_to_shop("../Salon")
@@ -100,19 +98,18 @@ func _ready():
 		visited_fx_kaiju[fx].visible = false
 
 func co_visit_shop(shop):
-	emit_signal("shop_entered", shop)
 	var mask = collision_mask
 	collision_mask = 0
 
 	var delta
-	var x = translation.z
-	var shopx = shop.translation.z
+	var z = translation.z
+	var shopz = shop.translation.z
 		
-	while x != shopx:
-		$HumanForm.look_at(translation + Vector3(shopx-x, 0, 0), Vector3.UP)
+	while z != shopz:
+		$HumanForm.look_at(translation + Vector3(shopz-z, 0, 0), Vector3.UP)
 		delta = yield()
-		x = move_toward(x, shopx, delta*MOVE_SPEED)
-		translation.x = x
+		z = move_toward(z, shopz, delta*MOVE_SPEED)
+		translation.z = z
 
 	$AnimationPlayer.play("enter_shop")
 	yield(get_tree(), "idle_frame")
@@ -139,12 +136,22 @@ func co_visit_shop(shop):
 	collision_mask = mask
 	emit_signal("shop_exited", shop)
 
+func _physics_process(delta):
+	if co_state is GDScriptFunctionState && co_state.is_valid():
+		co_state = co_state.resume(delta)
+	else:
+		co_state = null
+
 func visit_shop(shop):
 	if is_transformed and !in_knockback:
-		if not visited_shops.get(shop.name, false):
-			co_visit_shop(shop)
+		if !visited_shops[shop.name]:
+			co_state = co_visit_shop(shop)
+			emit_signal("shop_entered", shop)
 	
 func _on_shoparea_body_entered(body, shop):
 	if self == body and !visited_shops[shop.name]:
-		pass
-		# visit_shop(shop)
+		nearby_shop = shop
+
+func _on_shoparea_body_exited(body, shop):
+	if self == body and shop == nearby_shop:
+		nearby_shop = null
