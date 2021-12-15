3b37d2aea6ff4c189cd60e1eb69f9e6ac8d50423
IoriBranford <ioribranford@gmail.com>
2021-12-14 18:18:35 -0800

    Fix more error spam from Transform.looking_at.

--- scenes/MagicGirl.gd
+++ scenes/MagicGirl.gd
@@ -107,7 +107,7 @@ func _process(delta):
 				patrol_timer.start(patrol_time)
 				self.state = MagGirlState.PATROL
 			else:
-				smooth_look_at(Vector3(start_pos.x, 0, start_pos.z), Vector3.UP)
+				smooth_look_at(Vector3(start_pos.x, global_transform.origin.y, start_pos.z), Vector3.UP)
 				global_transform.origin = global_transform.origin.move_toward(start_pos, delta * MOVE_SPEED * 2)
 		MagGirlState.PATROL:
 			target_los = check_los()
@@ -164,7 +164,7 @@ func _process(delta):
 				emit_signal("patrol_done")
 			else:
 				global_transform.origin = global_transform.origin.move_toward(home_pos, delta * MOVE_SPEED * 2)
-			smooth_look_at(Vector3(home_pos.x, 0, home_pos.z), Vector3.UP)
+			smooth_look_at(Vector3(home_pos.x, global_transform.origin.y, home_pos.z), Vector3.UP)
 		MagGirlState.SWATTED:
 			if global_transform.origin == home_pos:
 				girl_data[girl_mode].model.rotation_degrees = Vector3.ZERO
